64c87dc486ea511557dc6a639095bfeaade626d7
neelk <neelesh.karthi@gmail.com>
2024-10-24 15:50:52 -0400

    add util.py

---
+++ utils.py
@@ -0,0 +1,123 @@
+import syft as sy
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from torch.utils.data import DataLoader, random_split
+from torchvision.datasets import CIFAR10
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+    
+    
+def imshow(img):
+    img = img / 2 + 0.5  # unnormalize
+    plt.imshow(np.transpose(img.numpy(), (1, 2, 0)))
+    plt.show()
+
+    
+def load_datasets(NUM_CLIENTS, MANUAL_SEED):
+    BATCH_SIZE = 32
+    
+    # Download and transform CIFAR-10 (train and test)
+    transform = transforms.Compose([transforms.ToTensor(), 
+                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    
+    trainset = CIFAR10("./dataset", train=True, download=True, transform=transform)
+    testset = CIFAR10("./dataset", train=False, download=True, transform=transform)
+
+    # Split training set into partitions to simulate the individual dataset
+    partition_size = len(trainset) // NUM_CLIENTS
+    lengths = [partition_size] * NUM_CLIENTS
+    datasets = random_split(trainset, lengths, torch.Generator().manual_seed(MANUAL_SEED))
+
+    # Split each partition into train/val and create DataLoader
+    trainloaders = []
+    valloaders = []
+    for ds in datasets:
+        len_val = len(ds) // 10  # 10 % validation set
+        len_train = len(ds) - len_val
+        lengths = [len_train, len_val]
+        ds_train, ds_val = random_split(ds, lengths, torch.Generator().manual_seed(MANUAL_SEED))
+        trainloaders.append(DataLoader(ds_train, batch_size=BATCH_SIZE, shuffle=True))
+        valloaders.append(DataLoader(ds_val, batch_size=BATCH_SIZE))
+    
+
+    # Split test set into partitions to simulate the individual dataset
+    partition_size = len(testset) // NUM_CLIENTS
+    lengths = [partition_size] * NUM_CLIENTS
+    datasets = random_split(testset, lengths, torch.Generator().manual_seed(MANUAL_SEED))
+
+    testloaders = []
+    for ds in datasets:
+        testloaders.append(DataLoader(ds, batch_size=BATCH_SIZE, shuffle=True))    
+    
+    return trainloaders, valloaders, testloaders
+
+def train(net, trainloader, epochs: int, verbose=False):
+    """Train the network on the training set."""
+    
+    DEVICE = torch.device("cpu")  # Try "cuda" to train on GPU
+    criterion = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(net.parameters())
+    net.train()
+    for epoch in range(epochs):
+        correct, total, epoch_loss = 0, 0, 0.0
+        for images, labels in trainloader:
+            images, labels = images.to(DEVICE), labels.to(DEVICE)
+            optimizer.zero_grad()
+            outputs = net(images)
+            loss = criterion(net(images), labels)
+            loss.backward()
+            optimizer.step()
+            # Metrics
+            epoch_loss += loss
+            total += labels.size(0)
+            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+        epoch_loss /= len(trainloader.dataset)
+        epoch_acc = correct / total
+        if verbose:
+            print(f"Epoch {epoch+1}: train loss {epoch_loss}, accuracy {epoch_acc}")
+
+
+def test(net, testloader):
+    """Evaluate the network on the entire test set."""
+    
+    DEVICE = torch.device("cpu")  # Try "cuda" to train on GPU
+    criterion = torch.nn.CrossEntropyLoss()
+    correct, total, loss = 0, 0, 0.0
+    net.eval()
+    with torch.no_grad():
+        for images, labels in testloader:
+            images, labels = images.to(DEVICE), labels.to(DEVICE)
+            outputs = net(images)
+            loss += criterion(outputs, labels).item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    loss /= len(testloader.dataset)
+    accuracy = correct / total
+    return loss, accuracy
